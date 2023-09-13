70da979c4332759b46c88ce66d3473797bbfd1d2
Mahbubur Rahman <mrahman623a@gmail.com>
2023-09-13 19:08:17 +0600

    NewFile

---
+++ CCTemplate.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+
+#define nl cout << endl
+#define PI M_PI
+#define Yes printf("Yes\n")
+#define No printf("No\n")
+#define YES printf("YES\n")
+#define NO printf("NO\n")
+
+#define scan(x, s)              \
+    for (int i = 0; i < s; i++) \
+        cin >> x[i];
+#define scanV(x, s)             \
+    for (int i = 0; i < s; i++) \
+    {                           \
+        int temp;               \
+        cin >> temp;            \
+        x.push_back(temp);      \
+    }
+#define print(x)              \
+    for (auto value : x)      \
+        cout << value << " "; \
+    cout << endl;
+
+void solve()
+{
+    
+}
+
+int main()
+{
+    solve();
+
+    return 0;
+}
