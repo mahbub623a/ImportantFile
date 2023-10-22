3df7435e5f6eda54c79359d9812078489402a4e0
Mahbubur Rahman <mrahman623a@gmail.com>
2023-10-23 01:43:16 +0600

    Updated

--- CPTemplate.cpp
+++ CPTemplate.cpp
@@ -19,6 +19,8 @@ typedef unsigned long long ull;
 #define Hacked return 0
 #define loop(i, s, n) for (int i = s; i < n; i++)
 #define rloop(i, n, s) for (int i = n - 1; i >= s; i--)
+#define sortV(v) sort(v.begin(), v.end())
+#define sortR(v) sort(v.rbegin(), v.rend())
 
 #define scan(x, s)              \
     for (int i = 0; i < s; i++) \
