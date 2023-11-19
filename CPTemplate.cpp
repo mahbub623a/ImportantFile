26965f1bc300e1d9f2b86ec022c26d0cbe127e2b
Mahbubur Rahman <mrahman623a@gmail.com>
2023-11-20 03:19:24 +0600

    Updated

--- CPTemplate.cpp
+++ CPTemplate.cpp
@@ -8,7 +8,7 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
-#define nl cout << endl
+#define nl printf("\n")
 #define Yes printf("Yes\n")
 #define No printf("No\n")
 #define YES printf("YES\n")
@@ -19,29 +19,42 @@ typedef unsigned long long ull;
 #define True printf("true\n")
 #define False printf("false\n")
 
-#define loop(i, s, n) for (int i = s; i < n; i++)
-#define rloop(i, n, s) for (int i = n - 1; i >= s; i--)
+#define sortV(v) sort(v.begin(), v.end())
+#define sortR(v) sort(v.rbegin(), v.rend())
 #define search(v, m) *(m(v.begin(), v.end()))
 #define convert(s, t) transform(s.begin(), s.end(), s.begin(), ::t)
 #define Hacked return 0
-#define sortV(v) sort(v.begin(), v.end())
-#define sortR(v) sort(v.rbegin(), v.rend())
+#define loop(i, s, n) for (int i = s; i < n; i++)
+#define rloop(i, n, s) for (int i = n - 1; i >= s; i--)
 
 #define scan(x, s)              \
     for (int i = 0; i < s; i++) \
         cin >> x[i];
-#define print(x)             \
-    for (auto value : x)     \
-        cout                 \
-            << value << " "; \
+#define print(x)            \
+    for (auto col : x)      \
+        cout << col << " "; \
     cout << endl;
+
+#define Scan(x, s1, s2)              \
+    for (int i = 0; i < s1; i++)     \
+    {                                \
+        for (int j = 0; j < s2; j++) \
+        {                            \
+            cin >> x[i][j];          \
+        }                            \
+    }
+#define Print(x)       \
+    for (auto row : x) \
+    {                  \
+        print(row);    \
+    }
 
 // const int h = 1e9 + 10;
 // int Hash[h];
 
 void Hack()
 {
-    // Start Hacking...
+    
 }
 
 int main()
